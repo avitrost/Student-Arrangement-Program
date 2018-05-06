be2b137b37f1716fed68b91a3bfc2275bccaa0cf
mTumm <39008851+mTumm@users.noreply.github.com>
2018-05-06 12:55:08 -0400

    Add files via upload

--- txtClientProject/src/txtClientProject/ExcelReader.java
+++ txtClientProject/src/txtClientProject/ExcelReader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -13,7 +14,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelReader {
 	XSSFWorkbook wb;
 	
-	public ExcelReader(File filePath) throws IOException{
+	public ExcelReader(File filePath) throws IOException, InvalidFormatException{
 		wb = new XSSFWorkbook(filePath);
 	}
 	
