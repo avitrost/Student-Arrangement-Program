2f731c07a7ec11837e098ef6cc3e33904392d309
avitrost <39007976+avitrost@users.noreply.github.com>
2018-06-03 14:03:05 -0400

    Add files via upload

--- txtClientProject/src/txtClientProject/SelectSessionScene.java
+++ txtClientProject/src/txtClientProject/SelectSessionScene.java
@@ -14,6 +14,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class SelectSessionScene extends Scene{
@@ -204,6 +205,8 @@ public class SelectSessionScene extends Scene{
 				}
 			}
 		}
+		Scene sortedStudentScene = new SortedStudentScene(new BorderPane(), groups);
+		Program.getWindow().setScene(sortedStudentScene);
 	}
 	
 	public void swap(ArrayList<Student> group, int index1, int index2){
